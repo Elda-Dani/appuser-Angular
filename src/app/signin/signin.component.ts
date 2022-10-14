bd3a628c80574da81d7e3c4e4cda5309f9a43b4a
Elda-Dani <eldadani2001@gmail.com>
2022-10-14 21:32:41 +0530

    login

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  constructor() { }
+
+  username=""
+  password=""
+  
+  
+  
+  readData=()=>{
+    let data={
+    "username":this.username,
+  "password":this.password
+    }
+    console.log(data)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
